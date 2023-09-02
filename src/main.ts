43d8790123a19ee5dbe133f15d097a6afc16e9d3
ifeanyi lucky <wfguru2017@gmail.com>
2023-09-02 22:47:54 +0100

    documentation set up

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './modules/app/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { InvalidFormExceptionFilter } from './filters/invalid.form.exception.filter';
 import { join } from 'path';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 // import cookieParser from 'cookie-parser';
 // import cors from 'cors';
 
@@ -19,6 +20,14 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.useGlobalFilters(new InvalidFormExceptionFilter());
 
+  const config = new DocumentBuilder()
+    .setTitle('FIRDAUS API DOC V1')
+    .setDescription('The Firdaus Gate API Documentation')
+    .setVersion('1.0')
+    .addTag('User')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   // app.use(cors());
   // app.use(cookieParser());
 
