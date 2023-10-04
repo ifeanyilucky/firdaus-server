76b84f7ef886d44625c5d83913bc28dfea40bf98
ifeanyi lucky <wfguru2017@gmail.com>
2023-10-04 23:57:11 +0100

    node_modules/puppeteer/install.js

--- src/utils/html-to-pdf.ts
+++ src/utils/html-to-pdf.ts
@@ -1,7 +1,17 @@
 import puppeteer from "puppeteer";
 
 export const htmlToPdf = async (html: string, outputPath: string) => {
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({
+    headless: "new",
+    executablePath: `/usr/bin/google-chrome`,
+    args: [
+      `--no-sandbox`,
+      `--headless`,
+      `--disable-gpu`,
+      `--disable-dev-shm-usage`,
+    ],
+  });
+
   const page = await browser.newPage();
 
   await page.setContent(html, {
